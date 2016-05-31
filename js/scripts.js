dd8b0d36057a7f5151a2f68d3bcde575b0be9353
Francisco Reynoso <francisco@dhcp-10-134-156-194.uawifi.arizona.edu>
2016-05-31 16:51:54 -0700

    did a lot of clean up

---
+++ js/scripts.js
@@ -0,0 +1,52 @@
+// Hide Header on on scroll down
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $('nav').outerHeight();
+
+$(window).scroll(function(event){
+    didScroll = true;
+});
+
+setInterval(function() {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+}, 250);
+
+function hasScrolled() {
+    var st = $(this).scrollTop();
+   
+    if(Math.abs(lastScrollTop - st) <= delta)
+        return;
+    if (st > lastScrollTop && st > navbarHeight){
+        // Scroll Down
+        $('nav').removeClass('nav-down').addClass('nav-up');
+    } else {
+        // Scroll Up
+        if(st + $(window).height() < $(document).height()) {
+            $('nav').removeClass('nav-up').addClass('nav-down');
+        }
+    }
+    
+    lastScrollTop = st;
+}
+
+// Prevent right click on images
+$('img').bind('contextmenu', function(e) {
+    return false;
+});
+
+// Jump to scroll
+$(document).ready(function() {
+    $(".scroll").on("click", function( e ) {
+
+        e.preventDefault();
+
+        $("body, html").animate({ 
+            scrollTop: $( $(this).attr('href') ).offset().top 
+        }, 600);
+
+    });
+});
